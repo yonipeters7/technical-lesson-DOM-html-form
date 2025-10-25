2c4707c6522b7c8a02ab886a3c896ab131e0ff12
Yonatan Peters <yonipeters7@gmail.com>
2025-10-25 14:59:43 -0400

    Technical Lesson DOM HTML form

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your code here!
+const shoppingList = [
+    {name: "Chicken Breast", price:10.50},
+    {name: "Paparika", price: 3.20}, 
+    {name: "Chips", price: 6.50}, 
+    {name: "Eggs", price: 4.00}, 
+    {name: "Milk", price: 7.80}
+ ]
+
+ function displayCartItem(item){
+   const shoppingCart = document.querySelector("#cart")
+   const newItem = document.createElement("li")
+
+   const itemName =document.createElement("p")
+   const itemPrice = document.createElement("p")
+
+   itemName.textContent = item.name
+	itemPrice.textContent = "$" + item.price
+
+   newItem.append(itemName, itemPrice)
+   shoppingCart.append(newItem)
+}
+
+ shoppingList.forEach(item => {
+        displayCartItem(item)
+    })
+
+    const form = document.querySelector("#new-cart-item-form")
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const newName = event.target.name.value
+     const newPrice = event.target.price.value
+
+	const newItem = {
+		name: newName,
+		price: newPrice
+	}
+	shoppingList.push(newItem)
+	displayCartItem(newItem)
+    }
+
+    form.addEventListener("submit", (event)=> {
+        handleSubmit(event)
+    })
